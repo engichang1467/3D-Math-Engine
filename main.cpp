7a2ece8bc0392020e4b80fb4bef3ab540cc13c35
Michael Chang <mchang0926@gmail.com>
2020-11-17 00:51:53 -0800

    new function to test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include "Vector3d.h"
+
+#include "Matrix3d.h"
 
 int main()
 {
     using namespace R3DMathEngine;
 
-    Vector3d vector1(2.0f, 3.0f, 1.0f);
-    Vector3d vector2(1.0f, 2.0f, 0.0f);
+    // Vector3d vector1(2.0f, 3.0f, 1.0f);
+    // Vector3d vector2(1.0f, 2.0f, 0.0f);
     
     /* Vector Addition */
     // Vector3d vector3 = vector1 + vector2;
@@ -44,8 +45,8 @@ int main()
     // cout << "Magnitude " << magnitude << endl;
 
     /* Normalize Vector */
-    vector1.normalize();
-    cout << vector1 << endl;
+    // vector1.normalize();
+    // cout << vector1 << endl;
     
     /* Vector Addition */
     // vector1 += vector2;
@@ -61,6 +62,65 @@ int main()
 
     // cout << "Vector 1 " << vector1 << endl;
     // cout << "Vector 2 " << vector2 << endl;
+
+    /* Matrix */
+
+    // Matrix3d m(2,3,1,
+    //            4,3,1,
+    //            5,3,1);
+
+    // Matrix3d a();
+
+    // a.show();
+
+    // Matrix3d m(2.0f, 3.0f, 1.0f, 
+    //            2.0f, 1.0f, 4.0f, 
+    //            5.0f, 3.0f, 2.0f);
+
+    // Matrix3d n(5.0f, 3.0f, 2.0f, 
+    //            2.0f, 1.0f, 1.0f, 
+    //            5.0f, 3.0f, 2.0f);
+
+
+    /* Adding matrices */
+    // Matrix3d res = m + n;
+    // m += n;
+
+    /* Subtracting matrices */
+    // Matrix3d res2 = m - n;
+    // m -= n;
+
+    /* Mutiplying Matrix (scalar) */
+    // Matrix3d res = m * 3;
+    // m *= 3;
+
+    /* Dividing Matrix (scalar) */
+    // Matrix3d res = m / 3;
+    // m /= 3;
+
+    /* Matrix Multiplication */
+    // Matrix3d res = m * n;
+    // m *= n;
+
+    /* Identity Matrix */
+    // m.setMatrixAsIdentityMatrix();
+
+    /* Inverse Matrix */
+    // Matrix3d res = m.getInverseOfMatrix();
+
+    /* Transpose */
+    // Matrix3d res = m.getTransposeOfMatrix();
+
+    /* Vector Transformation */
+    Matrix3d m(0.0f, 0.0f, 0.0f, 
+               0.0f, 0.0f, -1.0f, 
+               0.0f, 1.0f, 0.0f);
+
+    Vector3d vec(0,1,0);
+
+    Vector3d vec1 = m * vec;
+
+    cout << vec1 << endl;
 
     return 0;
 }
